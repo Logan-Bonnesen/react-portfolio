4fbbe48e54b302c9bb22f6e61fea9656de265083
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-02-16 17:25:14 -0700

    adding AboutMe avatar and updating footer text

--- react-porfolio/src/components/About.js
+++ react-porfolio/src/components/About.js
@@ -1,10 +1,14 @@
 import React from "react";
+import programmerIcon from "../img/programmer-icon.jpg"
 
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function About(props) {
   return (
+    <div>
+      <img className="img-responsive" src={programmerIcon}/>
     <p>I was born and raised in Phoenix but have been living in a small
     mountain town in Idaho for the past 6 years. My wife, Jess, is a nurse and we have an 11-month old daughter named Sloane. We also have a 13-year old husky mix named Lucy. We're back in Phoenix for a few months so my parents can help with childcare while I'm studying web development and my wife works a travel nurse assignment.</p>
+    </div>
   );
 }
 
--- react-porfolio/src/components/Footer.js
+++ react-porfolio/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function Footer(props) {
   return (
-    <p>This is the Footer</p>
+    <p>&copy; Logan Bonnesen 2022</p>
   );
 }
 
