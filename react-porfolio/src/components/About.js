b5b997aee24edea8f699834f59d713e7f70dad53
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2022-02-16 21:46:04 -0700

    updating photo avatar

--- react-porfolio/src/components/About.js
+++ react-porfolio/src/components/About.js
@@ -1,13 +1,14 @@
 import React from "react";
-import programmerIcon from "../img/programmer-icon.jpg"
+import programmerIcon from "../img/programmer-icon.jpg";
 
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function About(props) {
   return (
     <div>
-      <img className="img-responsive" src={programmerIcon}/>
-    <p>I was born and raised in Phoenix but have been living in a small
-    mountain town in Idaho for the past 6 years. My wife, Jess, is a nurse and we have an 11-month old daughter named Sloane. We also have a 13-year old husky mix named Lucy. We're back in Phoenix for a few months so my parents can help with childcare while I'm studying web development and my wife works a travel nurse assignment.</p>
+      <img className="img-responsive" src={programmerIcon} />
+      <p>
+        Hi, I'm Logan and I make things for the Internet.
+      </p>
     </div>
   );
 }
--- react-porfolio/src/components/Projects.js
+++ react-porfolio/src/components/Projects.js
@@ -4,30 +4,31 @@ import petRec from "../img/petRec.png";
 import movieRoulette from "../img/movieRoulette.png";
 import teamProfileGenerator from "../img/teamProfileGenerator.png";
 import employeeTracker from "../img/employeeTracker.png";
-import { Link } from "react-router-dom";
+
 
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function Projects(props) {
   return (
     <div>
-      <p>This is the header</p>
+      <p>Portfolio</p>
       <Container>
         <Row>
-         <a target="_blank" rel="noopener noreferrer" href="https://www.google.com/"><Col><img className="img-responsive" src={petRec} /></Col></a>
-         <a href="https://www.google.com/"><Col><img className="img-responsive" src={movieRoulette} target="_blank"/></Col></a>
-         <a href="https://www.google.com/"><Col><img className="img-responsive" src={teamProfileGenerator} target="_blank"/></Col></a>
+         <a target="_blank" rel="noopener noreferrer" href="https://pet-rec.herokuapp.com/"><Col><img className="img-responsive" src={petRec} /></Col></a>
+         <a target="_blank" rel="noopener noreferrer" href="https://logan-bonnesen.github.io/Movie-Roulette/"><Col><img className="img-responsive" src={movieRoulette}/></Col></a>
+         <a target="_blank" rel="noopener noreferrer" href="https://watch.screencastify.com/v/BEjGIIDbIX9jikwITtrd"><Col><img className="img-responsive" src={teamProfileGenerator}/></Col></a>
         </Row>
       </Container>
       <br></br>
       <Container>
         <Row>
-        <a href="https://www.google.com/"><Col><img className="img-responsive" src={employeeTracker}/></Col></a>
+        <a target="_blank" rel="noopener noreferrer" href="https://watch.screencastify.com/v/j5ZP6OiI8kEQgtlrfIrf"><Col><img className="img-responsive" src={employeeTracker}/></Col></a>
           <Col><img src="https://via.placeholder.com/150"/></Col>
           <Col><img src="https://via.placeholder.com/150"/></Col>
         </Row>
       </Container>
     </div>
   );
+
 }
 
 export default Projects;
